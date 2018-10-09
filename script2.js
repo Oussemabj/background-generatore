a654890c40e615b16c2887e0749c9d2643c7aab6
oussemabj <benjmeaaoussema@gmail.com>
2018-10-09 16:41:30 +0100

    add :p

---
+++ script2.js
@@ -0,0 +1,27 @@
+var	todos =[
+	"clean room",
+	"breakfest",
+	"sport",
+	"lol",
+	"blablabla",
+];
+/*var a = todos.length;
+for (var i = 0; i <a ; i++) {
+console.log(i);
+}*/
+todos.forEach(function(x,i)
+	{console.log(x,i);})
+
+/*
+var counterone=0;
+while(counterone<10)
+{
+	console.log(counterone);
+	counterone++
+}
+var counterone =10;
+while(counterone>0){
+	console.log(counterone);
+	counterone--;
+ 
+}*/
